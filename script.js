31b4a3f4f998f5d7f54975835f6bc217f18ef44b
nakkinn <81859514+nakkinn@users.noreply.github.com>
2021-08-01 23:39:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ function setup(){
     url[1]=createA('https://nakkinn.github.io/sketch1/','スケッチ１');
     url[1].position(30,190);
     url[1].style('font-size','25px');
+    url[2]=createA('https://nakkinn.github.io/maze/','迷路');
+    url[2].position(30,0);
+    url[2].style('font-size','25px');
 
     fractal(width/2,height,0,windowHeight/6,0);
     img=get(0,0,width,height);
