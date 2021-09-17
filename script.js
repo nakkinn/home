b811c64b30147307dd8ce146eca2bc610aa5469b
nakkinn <81859514+nakkinn@users.noreply.github.com>
2021-09-18 00:32:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,164 @@
-let title;
-let url=[];
-let m=0;
-let leaf=new Array(5000);
-for(let i=0;i<leaf.length;i++){
-    leaf[i]=new Array(10);
-}
-let img;
+let s=80;
+let board=new Array(5);
+for(let i=0;i<5;i++)    board[i]=new Array(8);
+for(let i=0;i<5;i++)    for(let j=0;j<8;j++)    board[i][j]=[];
+let enasite=new Array(5);
+for(let i=0;i<5;i++)    enasite[i]=new Array(8);
+let mode=0,c,r,turn,fturn,c1=-1,c2=-1,r1=-1,r2=-1;
+let peer,room;
 
 function setup(){
     createCanvas(windowWidth,windowHeight);
 
-    title=createP('なっきんの作品置き場')
-    title.position(20,-10);
-    title.style('font-size','50px');
-
-    url[0]=createA('https://nakkinn.github.io/othelo/','オセロ');
-    url[0].position(30,150);
-    url[0].style('font-size','25px');
-    url[1]=createA('https://nakkinn.github.io/sketch1/','スケッチ１');
-    url[1].position(30,190);
-    url[1].style('font-size','25px');
-    url[2]=createA('https://nakkinn.github.io/maze/','迷路');
-    url[2].position(30,230);
-    url[2].style('font-size','25px');
-    url[3]=createA('https://nakkinn.github.io/shogi/','将棋');
-    url[3].position(30,270);
-    url[3].style('font-size','25px');
-
-    fractal(width/2,height,0,windowHeight/6,0);
-    img=get(0,0,width,height);
+    for(let i=0;i<5;i++)    board[i][6][0]=0;
     
+    peer=new Peer({
+        key: 'cf1155ef-ab9f-41a3-bd4a-b99c30cc0663',
+        debug:1
+    });
+    peer.on('open',()=>{
+        id=peer.id;
+        room=peer.joinRoom("nokka",{
+            mode:'sfu'
+        });
+        room.on('open',()=>{
+            if(room.members.length==0)  turn=true;
+            else{
+                turn=false;
+                for(let i=0;i<5;i++)    board[i][1][0]=1;
+
+            }
+            fturn=turn;
+        });
+        room.on('peerJoin',peerId=>{
+            console.log(peerId+"参加");
+            if(turn)    for(let i=0;i<5;i++)    board[i][1][0]=1;
+        });
+        room.on('peerLeave',peerId=>{
+            console.log(peerId+"退出");
+        });
+        room.on('data',message=>{
+            console.log(message.data);
+            receive(message.data);
+        });
+    });
 }
 
 function draw(){
-    background(240);
-    image(img,0,0);
-    for(let i=0;i<m;i++){
-        noStroke();
-        fill(leaf[i][3],leaf[i][4],leaf[i][5],150);
-        circle(leaf[i][0],leaf[i][1],leaf[i][2]); 
-        
-        if(frameCount>1000&&random(1)<.00001)  leaf[i][6]=1;
-        if(leaf[i][6]==1){
-          leaf[i][0]+=10*(.5-noise(frameCount*.002,leaf[i][1]*.01));
-          leaf[i][1]+=2;
-        }
-      }
-}
-
-function fractal(x,y,a,r,n){
-  let p=x+r*cos(PI/2+a),q=y-r*sin(PI/2+a);
-  stroke(0);
-  strokeWeight(map(r,0,200,0,20));
-  line(x,y,p,q);
-  if(r<50){
-    for(let i=0;i<8;i++){
-      leaf[m][0]=x+random(p-x);
-      leaf[m][1]=y+random(q-y);
-      leaf[m][2]=random(50);
-      leaf[m][3]=random(155,255);
-      leaf[m][4]=random(100,200);
-      leaf[m][5]=random(50,100);
-      leaf[m][6]=0;
-      m++;  
-    }
-  }
-  if(n<8){
-    if(random(1)>.1)  fractal(p,q,a+random(PI/10,PI/5),r*random(.7,1),n+1);
-    if(random(1)>.1)  fractal(p,q,a-random(PI/10,PI/5),r*random(.7,1),n+1);
-  }
-}
-/*
-void keyPressed(){
-  if(keyCode==ENTER){
-    m=0;
     background(255);
-    fractal(width/2,height,0,200,0);
-    img=get(0,0,width,height);
-  }
+
+    noStroke(),fill(200);
+    rect(s,s,5*s,6*s);
+    stroke(0);
+    for(let i=0;i<6;i++)    line(s+i*s,s,s+i*s,7*s);
+    for(let i=0;i<7;i++)    line(s,s+i*s,6*s,s+i*s);
+
+    strokeWeight(8);
+    if(!turn)   line(s*0.8,s,s*0.8,s*4);
+    if(turn)    line(s*0.8,s*4,s*0.8,s*7);
+    strokeWeight(1);
+
+    for(let i=0;i<5;i++)    for(let j=0;j<8;j++){
+
+        if(enasite[i][j]){
+            stroke('#ff7700'),noFill(),strokeWeight(2);
+            rect(s+i*s,j*s,s,s);
+            stroke(0),strokeWeight(1);
+        }
+        noStroke(),fill(160);
+        if((i==c1&&j==r1)||(i==c2&&j==r2)){
+            rect(s+i*s+1,j*s+1,s-2,s-2);
+        }
+
+        stroke(0);
+        rectMode(CENTER);
+        for(let k=0;k<board[i][j].length;k++)    if(board[i][j][k]!=undefined){
+            if(board[i][j][k]==0)   fill('#7777ff');
+            else    fill('#ffffff');
+            rect(s+i*s+s/2,j*s+s/2,s*(0.8-0.2*k),s*(0.8-0.2*k),5);
+        }
+        rectMode(CORNER);
+    }
 }
 
-void mouseClicked(){
-  for(int i=0;i<m;i++){
-    if(random(1)<.002)  leaf[i][6]=1;
-  }
-}*/
+function mouseClicked(){
+
+    if(turn){
+        let flag=false;
+        if(mouseX>=s&&mouseX<s*6&&mouseY>=0&&mouseY<s*7){
+            for(let i=0;i<5;i++)    for(let j=0;j<8;j++){
+                if(mouseX>=(i+1)*s&&mouseX<(i+2)*s&&mouseY>=j*s&&mouseY<(j+1)*s){
+                    if(enasite[i][j]){
+                        board[i][j][board[i][j].length]=0;
+                        board[c][r].length--;
+                        turn=false;
+                        room.send(c+','+r+','+i+','+j);
+                        c1=c,r1=r,c2=i,r2=j;
+                        flag=true;
+                    }else{
+                        let len=board[i][j].length;
+                        if(len>0){
+                            if(board[i][j][len-1]==0){
+                                c=i,r=j;
+                                enable(i,j);
+                            }else   flag=true;
+                        }else   flag=true; 
+                    }   
+                }
+            }
+        }else   flag=true;
+        if(flag)    for(let i=0;i<5;i++)    for(let j=0;j<8;j++)    enasite[i][j]=false;
+    }
+}
+
+function keyPressed(){
+    if(key=='r'){
+        room.send("reset");
+        reset();
+    }
+}
+
+function enable(c,r){
+    let dir=[[-1,0],[1,0],[0,-1],[0,1],[-1,-1],[-1,1],[1,-1],[1,1]];
+    for(let i=0;i<5;i++)    for(let j=0;j<8;j++)    enasite[i][j]=false;
+    for(let i=0;i<8;i++){
+        if(ins(c+dir[i][0],r+dir[i][1])){
+            if(board[c+dir[i][0]][r+dir[i][1]].length<3)    enasite[c+dir[i][0]][r+dir[i][1]]=true;
+        }
+    }
+}
+
+function ins(c,r){
+    if(c>=0&&c<5&&r>=0&&r<7)    return true;
+    else    return false;
+}
+
+function receive(s){
+    if(s=="reset"){
+        reset();
+    }else   if(s=="close"){
+        room.close();
+    }else{
+        s=s.split(',');
+        for(let i=0;i<4;i++)    s[i]=int(s[i]);
+        s[0]=4-s[0];
+        s[1]=7-s[1];
+        s[2]=4-s[2];
+        s[3]=7-s[3];
+        c1=s[0],r1=s[1],c2=s[2],r2=s[3];
+        board[s[0]][s[1]].length--;
+        board[s[2]][s[3]].push(1);
+        turn=true;
+    }
+}
+
+function reset(){
+    for(let i=0;i<5;i++)    for(let j=0;j<8;j++)    board[i][j].length=0;
+    for(let i=0;i<5;i++){
+        board[i][1][0]=1;
+        board[i][6][0]=0;
+    }
+    turn=!fturn;
+    fturn=turn;
+    c1=c2=r1=r2=-1;
+}
